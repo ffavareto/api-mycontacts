aadfaaa889b9a28bfc573b5d0e1aa709b11c67e5
Lucas Santos <lucasfavaretosantos98@gmail.com>
2022-03-18 15:02:24 -0300

    finalizada

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    commonjs: true,
+    es2020: true,
+    node: true,
+  },
+  extends: [
+    'airbnb-base',
+  ],
+  parserOptions: {
+    ecmaVersion: 11,
+  },
+  rules: {
+    'linebreak-style': 0,
+    'class-methods-use-this': 'off',
+    'no-console': 'off',
+    'no-promise-executor-return': 'off',
+    'consistent-return': 'off',
+    camelcase: 'off',
+    'no-unused-vars': ['error', { argsIgnorePattern: 'next' }],
+  },
+};
